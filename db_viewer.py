c5eb32975c251d933f730b1264f11d854bb443f5
Solomon Nderitu <solomonstead@gmail.com>
2023-04-29 22:42:17 +0300

    Added e-mail service to the db_viewer. Didn't put e-mail password for obvious security reasons. To work on the database_viewer and add more in-depth features including calculating prices,profit/loss and other costs.

--- db_viewer.py
+++ db_viewer.py
@@ -1,6 +1,9 @@
 import sqlite3
 import numpy as np
 import time
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 with sqlite3.connect('chicken_farm.db') as db:
     curs=db.cursor()
@@ -20,8 +23,8 @@ def view():
     curs.execute(query)
     for i in curs.fetchall():
         chicken_population.append(i)
-    total=np.sum(chicken_population)
-    print("\nTotal Population: %d" %total)
+    total_chicken=np.sum(chicken_population)
+    print("\nTotal Population: %d" %total_chicken)
 
     print("""\n2.Eggs records: \n""")
     time.sleep(0.5)
@@ -36,8 +39,8 @@ def view():
         egg_totals.append(i)
     curs.execute("SELECT date from egg_collected WHERE id=1")
     first_date=curs.fetchall()
-    total=np.sum(egg_totals)
-    print(f"\nThe current total eggs collected since {first_date} is {total}")
+    total_eggs=np.sum(egg_totals)
+    print(f"\nThe current total eggs collected since {first_date} is {total_eggs}")
     time.sleep(2)
 
     print("""\n3.Feeds records: \n""")
@@ -46,6 +49,37 @@ def view():
     for i in curs.fetchall():
         print(i)
     time.sleep(2)
+
+    def emailer(email):
+        email_sender = 'cluckcontrol@gmail.com'
+        email_password=input('Enter password: ')
+        email_receiver=(email)
+
+        body= (f'The current  total chicken population is {total_chicken} and the current egg totals are {total_eggs}')
+
+        em=EmailMessage()
+        em['From']=email_sender
+        em['To']=email_receiver
+
+        em['Subject']='Farm Records'
+        em.set_content(body)
+
+        context=ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            try:
+                smtp.login(email_sender,email_password)
+                smtp.sendmail(email_sender,email_receiver,em.as_string())
+                print("All good sir")
+            except:
+                print('Error! Mail not sent')
+    
+    user_email=input('Enter your email to recieve a condensed form of the data:  ')
+    emailer(user_email)
+            
+
+
+
 
 
 
@@ -56,6 +90,7 @@ def main():
     print("Here is an overview of all your farm. Please enter which table you would like to see more information about.")
     time.sleep(1.5)
     view()
+    
     
 
 
