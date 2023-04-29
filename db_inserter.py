1406938a3448296c9b11fbdd0ddd369428e6f709
Solomon Nderitu <solomonstead@gmail.com>
2023-04-29 20:50:26 +0300

    Finished the db_inserter file. However, need to optimise it. Try: 1.Reducing repetitive code. 2.Avoid reading massive amounts of data at once. Try reading one at a time using fetchone.
    However, want to include email sending function.

--- db_inserter.py
+++ db_inserter.py
@@ -132,7 +132,124 @@ def deleter():
     
         
 
+def modifier():
+    print("""  WELCOME! HERE IS AN OVERVIEW OF THE TABLES. WHICH WOULD YOU LIKE TO MODIFY?
+                1. Chicken
+                  \n""")
+    cursor.execute("SELECT * FROM chicken_houses")
+    for i in cursor.fetchall():
+            print(i)
+
+    print("\n2. Eggs: \n")
+    cursor.execute("SELECT * FROM eggs_collected")
+    for i in cursor.fetchall():
+        print(i)
     
+    print("\n3.Feeds:\n")
+    cursor.execute("SELECT * FROM feeds")
+    for i in cursor.fetchall():
+            print(i)
+    
+    def house_table_modifier():
+        user_input=input("""Which column would you like to modify? 
+                            1.House Name
+                            2. House Population
+                            3. House Daily Feed
+                            4. Medication
+                            
+                            Enter here: """)
+        if user_input=="1":
+             house_id=int(input("\nEnter the id of the house you would like to change name: "))
+             new_name=input("Enter the new name: ")
+             cursor.execute("UPDATE chicken_houses SET name = ? WHERE id = ?", (new_name, house_id))
+             db.commit()
+        
+        elif user_input =="2":
+             house_id=int(input("\nEnter the id of the house you would like to change population: "))
+             new_population=eval(input("Enter the new population: "))
+             cursor.execute("UPDATE chicken_houses SET population = ? WHERE id = ?", (new_population, house_id))
+             db.commit()
+
+        elif user_input=="3":
+             house_id=int(input("\nEnter the id of the house you would like to change daily feed: "))
+             new_feeds=eval(input("Enter the new daily feed in terms of mkebes: "))
+             cursor.execute("UPDATE chicken_houses SET feeds = ? WHERE id = ?", (new_feeds, house_id))
+        
+        elif user_input=="4":
+             house_id=int(input("\nEnter the id of the house you would like to change medication: "))
+             new_medication=eval(input("Enter the new daily feed in terms of mkebes: "))
+             cursor.execute("UPDATE chicken_houses SET medication = ? WHERE id = ?", (new_medication, house_id))
+        else:
+             print("Sorry! Currently Unavailable!")
+        
+
+    def egg_table_modifier():
+        user_input=input("""\nWhich column would you like to modify? 
+                            1.Date
+                            2. House
+                            3. Number
+                            
+                            Enter here: """)
+        if user_input=="1":
+             house_id=int(input("\nEnter the id of the house you would like to change date: "))
+             new_date=eval(input("Enter the new date: "))
+             cursor.execute("UPDATE egg_collected SET date = ? WHERE id = ?", (new_date, house_id))
+             db.commit()
+        
+        elif user_input =="2":
+             house_id=int(input("\nEnter the id of the record you would like to change house collected: "))
+             new_house=eval(input("Enter the new house: "))
+             cursor.execute("UPDATE egg_collected SET house = ? WHERE id = ?", (new_house, house_id))
+             db.commit()
+
+        elif user_input=="3":
+             house_id=int(input("\nEnter the id of the house you would like to change number: "))
+             new_number=eval(input("Enter the new daily feed in terms of mkebes: "))
+             cursor.execute("UPDATE egg_collected SET number = ? WHERE id = ?", (new_number, house_id))
+             db.commit()
+        
+        else:
+             print("Sorry! Currently Unavailable!")
+
+    def feeds_table_modifier():
+        user_input=input("""\nWhich column would you like to modify? 
+                            1.Name
+                            2. Quantity
+                            3. Remaining
+                            
+                            Enter here: """)
+        if user_input=="1":
+             house_id=int(input("\nEnter the id of the record you would like to change name: "))
+             new_name=eval(input("Enter the new name: "))
+             cursor.execute("UPDATE feeds SET name = ? WHERE id = ?", (new_name, house_id))
+             db.commit()
+        
+        elif user_input =="2":
+             house_id=int(input("\nEnter the id of the record you would like to change quantity: "))
+             new_quantity=eval(input("Enter the new quantity: "))
+             cursor.execute("UPDATE feeds SET quantity = ? WHERE id = ?", (new_quantity, house_id))
+             db.commit()
+
+        elif user_input=="3":
+             house_id=int(input("\nEnter the id of the record you would like to change amount remaining: "))
+             new_remaining=eval(input("Enter the new amount remaining: "))
+             cursor.execute("UPDATE feeds SET remaining = ? WHERE id = ?", (new_remaining, house_id))
+             db.commit()
+       
+        else:
+             print("Sorry! Currently Unavailable!")   
+
+
+    user_in=input("\nEnter here: ")
+    if user_in=="1":
+         house_table_modifier()
+    elif user_in=="2":
+         egg_table_modifier()
+    elif user_in=="3":
+         feeds_table_modifier()
+    else:
+         print("Option Unavailable.")
+
      
 
 def main():
@@ -145,6 +262,8 @@ def main():
                     """)
     if option=="1":
         creator()
+    if option=="2":
+         modifier()
     elif option=="3":
          deleter()
         
